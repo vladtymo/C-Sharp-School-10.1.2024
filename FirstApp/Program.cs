ffa1c1862f31821054abce24e0c24b1fd4793b07
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-06 12:37:15 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// це є коментар
+
+// WriteLine - переносить вкінці текст на новий рядок
+// Write - не переносить текст на новий рядок
+
+// скорочення: cw + Tab = Console.WriteLine();
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("My name is Vlad!");
+
+Console.Write("Red");
+Console.Write("Green");
+Console.Write("Blue");
+
+// спец символи: \t \n \' \"
+
+Console.Write("\n\nHello\tTab1\t\tTab2\n");
+Console.WriteLine("We are studing \"C#\"");
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine("Glory to Ukraine!");
+
+Console.ResetColor();
+
+// для показу кирилиці
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Привіт Україно!");
