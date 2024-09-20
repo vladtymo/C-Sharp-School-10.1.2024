16bfdc436ab9271f02f7feacc8c9bbe3d0062a8a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-20 11:55:26 +0300

    Змінні та введення з консолі

---
+++ 02_readline/Program.cs
@@ -0,0 +1,23 @@
+﻿// -------------------- Введення в консоль --------------------
+Console.WriteLine("Enter your nickname: ");
+
+// створення змінної: тип ім'я = значення;
+
+// типи даних:
+// * int - ціле число
+// * double - дробове число
+// * string - рядок
+
+string login = Console.ReadLine();
+
+Console.WriteLine($"Hello, dear {login}!");
+
+// --------- Завдання ---------
+// користувач вводить милі, а програма відображає це значення в кіломентрах
+
+Console.Write("Enter miles: ");
+int miles = int.Parse(Console.ReadLine());
+
+// арифметичні оператори: + - * /
+
+Console.WriteLine($"Kilometers: {miles * 1.609344} km");
