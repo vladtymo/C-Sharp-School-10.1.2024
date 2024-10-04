9fe34b4d7da27ddde03ef6d24a33ace92771d02f
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-04 12:14:20 +0300

    Логічні оператори

--- 02_readline/Program.cs
+++ 02_readline/Program.cs
@@ -4,8 +4,11 @@ Console.WriteLine("Enter your nickname: ");
 // створення змінної: тип ім'я = значення;
 
 // типи даних:
-// * int - ціле число
+// * short - ціле число / 2 bytes / 
+// * int - ціле число / 4 bytes / 
+// * long - ціле число / 8 bytes / 
 // * double - дробове число
+// * decimal - дробове число / 16
 // * string - рядок
 
 string login = Console.ReadLine();
@@ -17,7 +20,14 @@ Console.WriteLine($"Hello, dear {login}!");
 
 Console.Write("Enter miles: ");
 int miles = int.Parse(Console.ReadLine());
-
-// арифметичні оператори: + - * /
+ 
+// арифметичні оператори: + - * / % (остача від ділення)
 
 Console.WriteLine($"Kilometers: {miles * 1.609344} km");
+
+// -------------- приклад роботи операторів
+Console.WriteLine($"10 + 5: {10 + 5}");
+//...
+Console.WriteLine($"10 % 5: {10 % 5}"); // 0
+Console.WriteLine($"10 % 5: {10 % 3}"); // 1
+Console.WriteLine($"10 % 5: {11 % 3}"); // 2
---
+++ 03_logic_operators/Program.cs
@@ -0,0 +1,30 @@
+﻿// logic operators: > < >= <= == !=
+// condition combine: && (і) || (або)
+
+Console.Write("Hours: ");
+int hours = int.Parse(Console.ReadLine()!);
+
+if (hours >= 0 && hours <= 23)
+{
+    Console.WriteLine("Right!");
+}
+else
+{
+    Console.WriteLine("Wrong!");
+}
+
+Console.Write("Year: ");
+int year = int.Parse(Console.ReadLine()!);
+
+// якщо блок має лише одну інструкцію, то фігурні дужки {} можна опустити
+if (year % 4 == 0)
+    Console.WriteLine("Leap year!");
+
+Console.Write("Your number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 7 == 0 || number == 777)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Your number is lucky!");
+}
