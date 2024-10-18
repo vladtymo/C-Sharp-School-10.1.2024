526a44dc07cafc2068f8567b471a2ec342572562
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-18 12:32:20 +0300

    Create simple puzzle game

---
+++ 05_array/Program.cs
@@ -0,0 +1,38 @@
+﻿// Робота з масивами
+
+int price1 = 1200;
+int price2 = 45;
+int price3 = 3400;
+int price4 = 99;
+
+Console.WriteLine(price1 + price2 + price3 + price4);
+
+int[] prices = {3, 5, 1200, 99, 456 };
+
+prices[2] = 1350;
+Console.WriteLine(prices[2]);
+
+for (int i = 0; i < 5; ++i)
+{
+    Console.Write(prices[i] + " ");
+}
+
+int[,] map = new int[3, 4]
+{
+    { 1, 2, 3, 4 },
+    { 3, 9, 0, 1 },
+    { 1, 2, 3, 2 }
+};
+
+Console.WriteLine(map[0, 0]); // 1
+Console.WriteLine(map[0, 3]); // 4
+
+Console.WriteLine("\n------ Matrix ------");
+for (int r = 0; r < 3; r++)
+{
+    for (int c = 0; c < 4; c++)
+    {
+        Console.Write(map[r, c] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ 06_puzzle_game/Program.cs
@@ -0,0 +1,53 @@
+﻿int zeroY = 2;
+int zeroX = 1;
+
+int[,] puzzle = new int[4, 4]
+{
+    { 4, 10, 9, 1 },
+    { 3, 2, 15, 5 },
+    { 8, 0, 7, 11 },
+    { 6, 12, 14, 13 }
+};
+PrintPuzzle();
+
+while (true)
+{
+    var key = Console.ReadKey().Key;
+
+    switch (key)
+    {
+        case ConsoleKey.LeftArrow:
+            Swap(ref puzzle[zeroY, zeroX], ref puzzle[zeroY, zeroX + 1]);
+            ++zeroX;
+            break;
+        case ConsoleKey.RightArrow:
+            Swap(ref puzzle[zeroY, zeroX], ref puzzle[zeroY, zeroX - 1]);
+            --zeroX;
+            break;
+    }
+
+    Console.Clear();
+    PrintPuzzle();
+}
+
+void PrintPuzzle()
+{
+    for (int r = 0; r < 4; r++)
+    {
+        for (int c = 0; c < 4; c++)
+        {
+            if (puzzle[r, c] == 0)
+                Console.Write("[  ] ");
+            else
+                Console.Write($"[{puzzle[r, c], 2}] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Swap(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
